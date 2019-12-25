293f19932689cca4b588d32b4ff7a76119e73b01
Torikun <muscovadodrop@gmail.com>
2019-12-25 16:05:20 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+function fib(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  
+  return fib(n - 1) + fib(n - 2);
+}
+
+const value = process.argv[2] || 0;
+for (let i = 0; i <= value; i++) {
+  console.log(fib(i));
+}
